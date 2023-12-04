f447a7d02f49b64c6947e0fb6a4cc372763e2871
Lewis Kai Ho Wong (k22027157) <k22027157@kcl.ac.uk>
2023-12-04 23:32:21 +0000

    complete thing1 exercise

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'thingproject.things',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'thingproject.urls'
+ROOT_URLCONF = 'thingproject.things.urls'
 
 TEMPLATES = [
     {
